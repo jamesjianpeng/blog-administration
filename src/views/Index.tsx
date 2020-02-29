bf6f497d97317d77e90d07b44cfbec10891961db
jamesjianpeng <jamesjianpeng@gmail.com>
2020-02-29 22:25:25 +0800

    perf: 修改文案

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Index extends React.Component {
   public render() {
     return (
-      <div> 我是小可爱的，啵啵 😘  </div>
+      <div> --- </div>
     );
   }
 }
